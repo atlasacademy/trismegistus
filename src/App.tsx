ff888222e31264ec94fb18fe560639cb34c6da91
LouizFC <louizfc@pm.me>
2022-08-13 11:44:04 -0300

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { IconBook } from "@tabler/icons";
+
+export function App() {
+  return (
+    <div className="container m-auto text-center text-lg text-gray-800">
+      <IconBook />
+      Trismegistus
+    </div>
+  );
+}
